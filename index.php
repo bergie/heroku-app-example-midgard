0cbf23bb0b1615cfd8c4800cf636c2ff530d3456
piotras <piotrek.pokora@gmail.com>
2013-01-18 15:28:45 +0100

    Print available midgard types

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
-echo "Hello World! \n";
 ini_set('display_errors',1);
 
-echo "Midgard2 installed: " . mgd_version();
+echo "<h1>Midgard2 installed: " . mgd_version() . "</h1>"; 
 
+$re = new ReflectionExtension("midgard2");
+$names = array();
 
+echo "<h2>Available types: </h2>";
+
+foreach ($re->getClasses() as $class_ref) {
+        echo " - " . $class_ref->getName() . "</br>";
+}
 
 ?>
